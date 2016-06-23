bcfb1551f0e02108f83bda4e81517c1fa50d8fbb
Qingkang Zhang <qzhang72@illinois.edu>
2016-06-22 21:01:28 -0700

    ziya on line 11

--- course.py
+++ course.py
@@ -8,7 +8,7 @@ import cookielib
 import re
 import getpass
 import time
-
+ziya
 zi ya 
 def getCourse(browser,crn):
     response = browser.open("https://ui2web1.apps.uillinois.edu/BANPROD1/bwckschd.p_disp_detail_sched?term_in=120168&crn_in="+str(crn))
